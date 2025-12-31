ee23d4463056d69b8e25f04d8076b98de3f396a6
rainyan <3530661536@qq.com>
2025-12-31 18:59:34 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -22,7 +22,7 @@ const SITE_LANG = "zh_CN"; // 语言代码，例如：'en', 'zh_CN', 'ja' 等。
 const SITE_TIMEZONE = 8; //设置你的网站时区 from -12 to 12 default in UTC+8
 export const siteConfig: SiteConfig = {
 	title: "颜",
-	subtitle: "真是个笨蛋",
+	subtitle: "2026新年快乐！",
 	siteURL: "https://blog.shinyan.top/", // 请替换为你的站点URL，以斜杠结尾
 	siteStartDate: "2025-12-26", // 站点开始运行日期，用于站点统计组件计算运行天数
 
